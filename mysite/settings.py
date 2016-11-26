dab901f057d3ad337c2731cd23dbc526e7948e7d
Orsolia <kisorsolia@gmail.com>
2016-11-26 16:01:06 +0100

    message

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%rl(z+9_$z#*pe7mnp@ff7psu&_tc=-om5=$w0-^77sc2phae$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kissorsolya.pythonanywhere.com']
 
 
 # Application definition
